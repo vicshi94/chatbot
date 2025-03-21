193fa36fef4a4258c338d6e7152933cda4673e03
Xuanyu SHI <79785997+vicshi94@users.noreply.github.com>
2025-03-21 17:53:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.sidebar:
     if not(hf_uid.isdigit() and int(hf_uid)>=1000 and int(hf_uid)<=9999):
         st.warning('请登录!用户ID必须为4位数字', icon='⚠️')
     else:
-        if str(hf_uid) == "0011":
+        if hf_uid == 1014:
             flag = True
         st.success('Enjoy the conversation!', icon='🤗')
     st.markdown(
